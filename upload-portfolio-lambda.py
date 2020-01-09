3667f37566e9f05b954621f2c0e8f99a792d8a09
Joseph James <theCubeOfSpheres@gmail.com>
2020-01-09 14:09:58 -0500

    portfolio build script for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,21 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.architectsandbox.net')
-build_bucket = s3.Bucket('portfoliobuild.architectsandbox.net')
-
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfolioartifact.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        mime_type = mimetypes.guess_type(nm)[0]
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': str(mime_type)})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+    portfolio_bucket = s3.Bucket('portfolio.architectsandbox.net')
+    build_bucket = s3.Bucket('portfoliobuild.architectsandbox.net')
+    
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfolioartifact.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            mime_type = mimetypes.guess_type(nm)[0]
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': str(mime_type)})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    # DeployPortfolio
+    return 'Hello From Lambda'
